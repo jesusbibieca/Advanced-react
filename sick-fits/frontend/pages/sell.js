d00d2cd3ad33f3d9be27e2392abd151c01b0576d
Jesus Bibieca <jesusbibieca52@gmail.com>
2018-10-22 22:11:18 -0400

    Initial boiler plate - Adding home, sell pages, meta tags, icon. Created Header and Nav component

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sell extends Component {
+  render() {
+    return (
+      <div>
+        <p>Sell component here</p>
+      </div>
+    );
+  }
+}
+
+export default Sell;
